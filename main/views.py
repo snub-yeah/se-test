29a2e7b7e8086b2fe68a9659433ad094d2790461
snub-yeah <wil.luke985@gmail.com>
2026-02-05 11:23:25 -0600

    basic views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST.get("username"),
+            password=request.POST.get("password"),
+        )
+        if user:
+            login(request, user)
+            return redirect("home")
+
+    return render(request, "main/login.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        user = User.objects.create_user(
+            username=request.POST.get("username"),
+            email=request.POST.get("email"),
+            password=request.POST.get("password"),
+        )
+        login(request, user)
+        return redirect("home")
+
+    return render(request, "main/signup.html")
